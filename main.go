7d6afe1a882dd324e8b79ac762c54344a933182e
Kieron Browne <kbrowne@pivotal.io>
2019-04-23 16:24:02 +0100

    output child process exit code
    with some help from onsi's gexec.Session

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
 func main() {
+	var exitCode int
+
 	switch os.Args[1] {
 	case "run":
-		parent()
+		exitCode = parent()
 	case "child":
-		child()
+		exitCode = child()
 	default:
 		panic("wat should I do")
 	}
+	os.Exit(exitCode)
 }
 
-func parent() {
+func parent() int {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
@@ -27,20 +29,27 @@ func parent() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
+	cmd.Run()
+	return getExitCode(cmd)
 }
 
-func child() {
+func child() int {
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
+	cmd.Run()
+	return getExitCode(cmd)
+}
+
+func getExitCode(cmd *exec.Cmd) int {
+	status := cmd.ProcessState.Sys().(syscall.WaitStatus)
+	var exitCode int
+	if status.Signaled() {
+		exitCode = 128 + int(status.Signal())
+	} else {
+		exitCode = status.ExitStatus()
 	}
+	return exitCode
 }
