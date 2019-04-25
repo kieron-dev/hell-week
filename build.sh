02a621e6a1a3896caac0c7f4bd2e38bb466459d0
Kieron Browne <kbrowne@pivotal.io>
2019-04-25 12:59:45 +0100

    rootfs-make to create an overlay filesystem

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ export GOARCH=amd64
 go build -v -o hell github.com/kieron-pivotal/hell-week/container-run
 go build -v -o pin-cpu github.com/kieron-pivotal/hell-week/pincpu
 go build -v -o limit-memory github.com/kieron-pivotal/hell-week/limitmem
+go build -v -o rootfs-make github.com/kieron-pivotal/hell-week/rootfsmake
---
+++ rootfsmake/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	flag "github.com/spf13/pflag"
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	image := flag.String("image", "", "path to base image")
+	graph := flag.String("graph", "", "path to an empty, temporary directory")
+	flag.Parse()
+
+	if *image == "" || *graph == "" {
+		panic("eh?")
+	}
+	mustExist(*image)
+	mustExist(*graph)
+
+	mergedDir := makeLayeredRootFS(*image, *graph)
+	fmt.Print(mergedDir)
+}
+
+func makeLayeredRootFS(image, graph string) string {
+	location, err := ioutil.TempDir("/tmp", "rootfs")
+	if err != nil {
+		panic(err)
+	}
+	workDir, err := ioutil.TempDir("/tmp", "work")
+
+	data := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", image, graph, workDir)
+	must(unix.Mount("overlay", location, "overlay", 0, data))
+	return location
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustExist(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		panic(err)
+	}
+}
